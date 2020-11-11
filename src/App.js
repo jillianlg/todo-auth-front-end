d006ac8987c2243270d659458728eb3a78b067f0
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-11-11 14:24:03 -0800

    add home and auth pages, add routes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+    Link,
+} from "react-router-dom";
+import './App.css';	import './App.css';
+import Auth from './Auth.js'
+import Home from './Home.js'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <ul>
+            <Link to="/auth"><div>Log In</div></Link>
+          </ul>
+          <Switch>
+            <Route exact path='/' render={(routerProps)=> <Home {...routerProps} />} />
+            <Route exact path='/auth' render={(routerProps)=> <Auth {...routerProps} />} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
