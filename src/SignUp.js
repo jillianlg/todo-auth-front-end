87a08aee8f8e71a8a7e7ddccca970ca30515cd0e
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-11-11 15:17:04 -0800

    add POST and test in postman

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import './App.css';	import './App.css';
+import request from 'superagent';
 
 export default class Signup extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        loading: false,
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async(e) => {
         e.preventDefault();
 
         console.log(this.state);
+
+        this.setState({ loading:true })
+        const user = await request
+            .post('https://cryptic-shore-29263.herokuapp.com/auth/signup')
+            .send(this.state); 
+
+
+        console.log(user.body, 'sending you to todos');
+        this.setState({ loading: false })
     }
 
     render() {
